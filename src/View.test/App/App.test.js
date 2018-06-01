ce2050b1eb27d4c3536a3b34d7cf5224ecdbb91b
nicola <nicola@zoyi.co>
2018-06-02 01:49:48 +0900

    App.test.js 잘못된것 수정

--- src/View.test/App/App.test.js
+++ src/View.test/App/App.test.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import App from '../../view/App/App.js';
-import {shallow} from 'enzyme';
+import {BrowserRouter} from "react-router-dom";
+import {mount} from 'enzyme';
+import {Provider} from "react-redux";
+import store from "../../Redux/Store";
+
+import App from '../../View/App/App.js';
 
 
 describe('App Test', () => {
   describe('Render Test', () => {
     it ('Main Div가 렌더링 되어야 한다.', () => {
-      let result = shallow(<App />)
-      expect(result.find('#Main')).toHaveLength(0);
+      const result = mount(
+        <Provider store={store}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </Provider>
+      )
+      expect(result.find('#Main')).toHaveLength(1);
     });
   });
 });
