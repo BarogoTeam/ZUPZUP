bdb181f953fdb8bffcebd6110b4273e9a6e54149
Conan Kim and Heejun Jang <crian0121+vsv21gml@gmail.com>
2018-06-16 18:05:04 +0900

    [Bug-Fix] Router 내 AlarmsPage 반영

--- src/Router/BasicRouter.js
+++ src/Router/BasicRouter.js
@@ -1,15 +1,15 @@
 import {Route, Switch} from "react-router-dom";
 import React, {Component} from "react";
 import MoviesPage from "../View/MoviesPage/MoviesPage";
-import MainPage from "../View/MainPage/MainPage";
+import AlarmsPage from "../View/AlarmsPage/AlarmsPage";
 
 class BasicRouter extends Component {
   render() {
     return (
       <main>
         <Switch>
-          <Route exact path="/" component={MainPage} />
-          <Route path="/alarms" component={MainPage} />
+          <Route exact path="/" component={AlarmsPage} />
+          <Route path="/alarms" component={AlarmsPage} />
           <Route path="/movies" component={MoviesPage} />
         </Switch>
       </main>
