0b24a875efbd067518cad5bdb3f1174c451ebd65
Conan Kim and Heesun Kim <crian0121+heesunbe@gmail.com>
2019-01-12 15:41:08 +0900

    [Chores] AlarmService 코드 정리

--- src/Service/AlarmService.js
+++ src/Service/AlarmService.js
@@ -8,50 +8,48 @@ function objToQueryString(obj) {
   return keyValuePairs.join('&');
 }
 
+const Header = new Headers();
+Header.append("Content-Type", "application/json");
+
 export default class AlarmService {
-
   static getCinemas() {
-    const Header = new Headers();
-    Header.append("Content-Type", "application/json");
-
-    const myInit = {
-      method: 'GET',
+    return fetch(`${BACKEND_URL}/cinemas`, {
+      method: "GET",
       headers: Header
-    };
-
-    return fetch(`${BACKEND_URL}/cinemas`, myInit).then((response) => {
+    }).then((response) => {
       return response.json();
     })
   }
+
   static getScreens(cinemas, alarmDate) {
-
-    const Header = new Headers();
-    Header.append("Content-Type", "application/json");
-
-    const myInit = {
-      method: 'GET',
-      headers: Header
-    };
     const queryString = objToQueryString({
       cinemaIds : cinemas.map(cinema => `${cinema.divisionCode}|${cinema.detailDivisionCode}|${cinema.code}`).join(','),
       alarmDate: alarmDate.format('YYYY-MM-DD')
     });
-    return fetch(`${BACKEND_URL}/screens?${queryString}`, myInit).then((response) => {
+
+    return fetch(`${BACKEND_URL}/screens?${queryString}`, {
+      method: "GET",
+      headers: Header
+    }).then((response) => {
       return response.json();
     })
   }
 
   static getMovie(movieCode) {
-    const Header = new Headers();
-    Header.append("Content-Type", "application/json");
-
-    const myInit = {
-      method: 'GET',
+    return fetch(`${BACKEND_URL}/movies/${movieCode}`, {
+      method: "GET",
       headers: Header
-    };
-    return fetch(`${BACKEND_URL}/movies/${movieCode}`, myInit).then((response) => {
+    }).then((response) => {
       return response.json();
     })
   }
 
+  static getSeats(screens) {
+    return fetch(`${BACKEND_URL}/seats/${JSON.stringify}`, {
+      method: "GET",
+      headers: Header
+    }).then((response) => {
+      return response.json();
+    })
+  }
 }
