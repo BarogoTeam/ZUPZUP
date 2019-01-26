9396302e92d270ab5b414278e139f41418bff122
Conan Kim and Thesun Kim <crian0121+thesun4sky@gmail.com>
2019-01-26 12:08:25 +0900

    [Story] Alarms 페이지 출력 UI개선중

--- src/Component/AlarmListItem/AlarmListItem.js
+++ src/Component/AlarmListItem/AlarmListItem.js
@@ -12,39 +12,40 @@ class AlarmListItem extends React.PureComponent {
   toggleRunMode = () => this.setState({isRun: !this.state.isRun})
 
   render() {
-    const weekDays = ['일', '월', '화', '수', '목', '금', '토'];
+    //const weekDays = ['일', '월', '화', '수', '목', '금', '토']; TODO 요일설정은 나중에  by thesun.kim
 
     return (
-      <UI.Item >
-        <UI.Item.Image disabled={!this.state.isRun} size='large' src={this.props.alarmInfo.img} />
+      <UI.Grid.Column>
+        <UI.Image fluid
+                       label={{ as: 'a', color: 'red', content: '롯데시네마', ribbon: true}}
+                       disabled={!this.state.isRun} size='large' src={this.props.alarmInfo.img} />
         <UI.Item.Content>
-          <UI.Item.Header>{this.props.alarmInfo.name}</UI.Item.Header>
+          <UI.Header size='large'><UI.Header.Subheader><br/></UI.Header.Subheader>{this.props.alarmInfo.name}
+          </UI.Header>
           <UI.Item.Extra>
             <div>
               {this.props.alarmInfo.cinemaNames.map((cinemaName) => <UI.Label key={cinemaName}>{cinemaName}</UI.Label>)}
             </div>
             <div>
-              {this.props.alarmInfo.cinemaTypes.map((cinemaType) => <UI.Label key={cinemaType}>{cinemaType}</UI.Label>)}
+              {this.props.alarmInfo.cinemaTypes.map((cinemaType) => <UI.Label key={cinemaType}>{cinemaType}관</UI.Label>)}
             </div>
             <div>
-              <UI.Label><UI.Icon name={this.props.alarmInfo.reservationNumber === 1 ? "user" : "users"} />{this.props.alarmInfo.reservationNumber}</UI.Label>
-              <UI.Label><UI.Icon name="calendar alternate outline" />{this.props.alarmInfo.startDate}</UI.Label>
-              <UI.Label><UI.Icon name="calendar times outline" />{this.props.alarmInfo.endDate}</UI.Label>
+              <UI.Label tag color='black'><UI.Icon name={this.props.alarmInfo.reservationNumber === 1 ? "user" : "users"} />{this.props.alarmInfo.reservationNumber}</UI.Label>
+              <UI.Label tag color='teal'><UI.Icon name="calendar alternate outline" />{this.props.alarmInfo.date}</UI.Label>
             </div>
             <div>
-              <UI.Label><UI.Icon name="check circle outline" />{this.props.alarmInfo.startTime}</UI.Label>
-              <UI.Label><UI.Icon name="times circle outline" />{this.props.alarmInfo.endTime}</UI.Label>
+              <UI.Label tag color='blue'><UI.Icon name="check circle outline" />{this.props.alarmInfo.startTime}</UI.Label>
+              <UI.Label tag color='gray'><UI.Icon name="times circle outline" />{this.props.alarmInfo.endTime}</UI.Label>
             </div>
-            <div>
+            {/*<div>
               {this.props.alarmInfo.weekDays.map((day) => <UI.Label key={day}>{weekDays[day]}</UI.Label>)}
-            </div>
+            </div>*/}
             <div className="ui right floated">
               <UI.Checkbox toggle checked={this.state.isRun} onClick={this.toggleRunMode} />
             </div>
           </UI.Item.Extra>
-
         </UI.Item.Content>
-      </UI.Item>
+      </UI.Grid.Column>
     )
   }
 }
--- src/View/AlarmsPage/AlarmsPage.js
+++ src/View/AlarmsPage/AlarmsPage.js
@@ -2,58 +2,77 @@ import React from 'react';
 import * as UI from 'semantic-ui-react';
 import {Link} from "react-router-dom";
 import AlarmListItem from '../../Component/AlarmListItem/AlarmListItem';
-
-const alarms = [
+import AlarmService from "../../Service/AlarmService";
+const alarms_temp = [
   {
-    img: "/resources/SAMPLE_IMAGE.JPG",
-    name: "어벤져스 인피니티워",
+    img: "http://caching.lottecinema.co.kr//Media/MovieFile/MovieImg/201901/13681_201_1.jpg",
+    name: "모마이",
     cinemaNames: ["가산디지털단지", "용아맥", "롯데월드점"],
-    cinemaTypes: ["2D", "3D", "IMAX"],
+    cinemaTypes: ["IMAX"],
     startTime: "18:00",
     endTime: "20:00",
-    startDate: "2018-01-21",
-    endDate: "2019-01-03",
+    date: "2019-01-21",
     weekDays: [1, 5, 6],
     reservationNumber: 1,
     isRun: true
   },{
-    img: "/resources/SAMPLE_IMAGE.JPG",
-    name: "데드풀2 번역가 갓",
+    img: "http://caching.lottecinema.co.kr//Media/MovieFile/MovieImg/201901/12956_201_1.jpg",
+    name: "드래곤 길들이기 3",
     cinemaNames: ["가산디지털단지", "용아맥", "롯데월드점"],
-    cinemaTypes: ["2D", "3D", "IMAX"],
+    cinemaTypes: ["2D", "4DX"],
     startTime: "18:00",
     endTime: "20:00",
-    startDate: "2018-05-12",
-    endDate: "2018-07-17",
+    date: "2019-01-21",
     weekDays: [2, 3, 4],
     reservationNumber: 6,
     isRun: true
   },{
-    img: "/resources/SAMPLE_IMAGE.JPG",
-    name: "어벤져스 베리의탄생",
+    img: "http://caching.lottecinema.co.kr//Media/MovieFile/MovieImg/201812/13438_203_1.jpg",
+    name: "아쿠아맨",
     cinemaNames: ["롯데월드점"],
-    cinemaTypes: ["1D"],
+    cinemaTypes: ["3D"],
     startTime: "22:00",
     endTime: "23:59",
-    startDate: "2018-06-14",
-    endDate: "2019-08-26",
+    date: "2019-01-21",
     weekDays: [1, 5],
     reservationNumber: 2,
     isRun: false
   }
 ]
 
+
 class AlarmsPage extends React.PureComponent {
+
+
+
+  constructor() {
+    super();
+    this.state = { alarms: [] };
+    console.log('hello');
+    AlarmService.getAlarms('ZupzupCrawler@zupzup.com')
+      .then( (alarms)=> {
+        if (alarms.size() == 0)
+          this.setState(alarms);
+        else
+          this.setState(alarms_temp);
+      });  //userkey 하드코딩 박아놓음 TODO 텔레그램방 번호로 수정필요 by thesun.kim
+  }
+
+  componentDidMount() {
+
+  }
+
   render() {
+    const alarms = this.state.alarms;
     return (
       <div>
-        <UI.Item.Group divided>
+        <UI.Grid columns={2}>
           {
             alarms.map((alarmInfo) =>
               <AlarmListItem key={alarmInfo.name} alarmInfo={alarmInfo} />
             )
           }
-        </UI.Item.Group>
+        </UI.Grid>
         <Link to="/alarms/new">
           <UI.Button fluid basic>
             <UI.Icon name="plus" />
