b3751e0b3f8250ce383b68dbb77716ec809e7d54
heejun <vsv21gml@naver.com>
2019-04-02 00:20:53 +0900

    api url change hj home

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,6 @@
 //export const BACKEND_URL = "https://etrn16p7n7.execute-api.ap-northeast-2.amazonaws.com/dev";
-export const BACKEND_URL = "http://localhost:4000";
+//export const BACKEND_URL = "http://localhost:4000";
+export const BACKEND_URL = "http://183.101.136.11:4000";
 
 export const MOVIE = {
   ACTION: {
